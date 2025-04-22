d71d5999d96c6e93156126e22d1d03cc79011717
oninotna <antoninomusarratubbi@gmail.com>
2025-04-22 14:45:40 +0200

    snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+const studentSearching = students.find((person) => person.id === 2);
+
+console.log(studentSearching);
