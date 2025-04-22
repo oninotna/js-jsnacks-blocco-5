ff05f9d8ab55bd8b349d08ccacf8591025d8df69
oninotna <antoninomusarratubbi@gmail.com>
2025-04-22 14:14:31 +0200

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,4 +6,4 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
-console.log();
+const peopleNames = people.forEach((person) => console.log(person.name));
